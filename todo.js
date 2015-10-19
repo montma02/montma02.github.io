dd70e46d785adecf0ee64086d32ac940bd1e0a45
montma02 <montma02@luther.edu>
2015-10-19 14:29:06 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -3,8 +3,13 @@
    document.querySelector("#tasklist")
    newIn = document.createElement("input")
    newIn.type = "checkbox"
-   
-   
+   li.appendChild("newIn")
+   inputid = document.querySelector("#inputid")
+   ul = document.querySelector("ul")
+   li = document.createElement("li")
+   task = document.createTextNode(inputid.value)
+   li.appendChild(task)
+   ul.appendChild(li)
     
   }
 
