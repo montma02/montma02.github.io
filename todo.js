e49b916c87a64ba7e3ea52b1321b934099729e73
montma02 <montma02@luther.edu>
2015-10-21 13:51:44 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -5,6 +5,7 @@ addTask = function() {
  check = document.createElement("input");
  check.type = "checkbox";
  check.name = "done";
+ checkbox.onclick = boxChecked;
  entry = document.querySelector("#word").value;
  litext = document.createTextNode(entry)
  priority = document.querySelector("#priority").value;
@@ -12,4 +13,14 @@ addTask = function() {
  newli.appendChild(check);
  newli.appendChild(litext);
  ul.appendChild(newli);
+}
+boxChecked = function() {
+    if (this.checked) {
+   this.parentNode.classList.add("done")
+   } else {
+    this.parentNode.classList.remove("done")
+   }
+   // local Save(id of your ul as a string)
+   // resoreList(id of your UL as a string, boxChecked)
+   // <sript type="text/javascript"> window.onload = function() { restoreLi("taskList", boxChecked)</script>
 }
