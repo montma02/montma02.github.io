5cb0e4997bc34573880e70f07bb789422b5ef199
montma02 <montma02@luther.edu>
2015-10-22 13:24:37 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,19 +1,21 @@
-addToDo = function() {
+addTask = function() {
  ul = document.querySelector("#tdlist")
  newli = document.createElement("li");
  check = document.createElement("input");
  check.type = "checkbox";
  check.name = "done";
  check.onclick = checkItem;
+ 
  entry = document.querySelector("#word").value;
- litxt = document.createTextNode(entry)
+ litext = document.createTextNode(entry)
  priority = document.querySelector("#priority").value;
  newli.className = priority;
  newli.appendChild(check);
- newli.appendChild(litxt);
+ newli.appendChild(litext);
  ul.appendChild(newli);
  localSave("tdlist");
 }
+
 checkItem = function() {
  if (this.checked){
   this.parentNode.classList.add("done");
